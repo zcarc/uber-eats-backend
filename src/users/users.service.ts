cdca916dcdfaf05aca931db2c70d893c1d2b7b4d
Hyunsoo Lee <wrkbrs@gmail.com>
2021-05-20 11:27:10 +0900

    JWT 생성

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,12 +4,15 @@ import { Repository } from 'typeorm';
 import { CreateAccountInput } from './dtos/create-account.dto';
 import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
+import * as jwt from 'jsonwebtoken';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly users: Repository<User>,
+    private readonly config: ConfigService,
   ) {}
 
   async createAccount({
@@ -48,6 +51,7 @@ export class UsersService {
           error: 'Wrong password',
         };
       }
+      const toke = jwt.sign({ id: user.id }, this.config.get('SECRET_KEY'));
       return {
         ok: true,
         token: 'lalalala',
