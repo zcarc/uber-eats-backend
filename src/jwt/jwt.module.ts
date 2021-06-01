4a86ede1faf20834e6b00f1d3f3fa54dcfdd4ccd
Hyunsoo Lee <wrkbrs@gmail.com>
2021-06-02 04:29:41 +0900

    JWT module 2

--- src/jwt/jwt.module.ts
+++ src/jwt/jwt.module.ts
@@ -1,14 +1,16 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
 import { JwtService } from './jwt.service';
+import { JwtModuleOptions } from './jwt.interfaces';
+import { CONFIG_OPTIONS } from './jwt.constants';
 
 @Module({})
 @Global()
 export class JwtModule {
-  static forRoot(): DynamicModule {
+  static forRoot(options: JwtModuleOptions): DynamicModule {
     return {
       module: JwtModule,
+      providers: [{ provide: CONFIG_OPTIONS, useValue: options }, JwtService],
       exports: [JwtService],
-      providers: [JwtService],
     };
   }
 }
